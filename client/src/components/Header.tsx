328d92ce10da67d52bb5280e908d105ec89bc9cd
Aditya Verma <aadiiver@gmail.com>
2025-03-20 20:50:57 +0530

    Initial frontend

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+const Header: React.FC = () => {
+  return (
+    <AppBar position="fixed" sx={{ width: '100%' }}>
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ fontWeight: 700, marginLeft: 5 }}>
+          RepoMind
+        </Typography>
+        <IconButton
+          edge="end"
+          color="inherit"
+          aria-label="github"
+          href="https://github.com"
+          target="_blank"
+          sx={{ marginLeft: 'auto', marginRight: 5, '&:hover': { color: 'white' } }}
+        >
+          <GitHubIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
